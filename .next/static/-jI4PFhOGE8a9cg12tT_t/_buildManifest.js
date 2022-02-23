44469777afe1a2216cce3111e6303a0d1a5d4387
Minh <cungminh27@gmail.com>
2022-02-24 09:14:21 +1100

    remove .next

--- .next/static/-jI4PFhOGE8a9cg12tT_t/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/403-fd2aeea87411f70a.js","static/chunks/pages/index-6a2cb081a049b811.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
